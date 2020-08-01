a8ade0ccbbd07edcb6f6d9fdeb0821a5835a7cf0
Jason Ji <uberjason@gmail.com>
2020-08-01 11:59:32 -0400

    Initial Commit

---
+++ PlaceholderCustomFontBug/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PlaceholderCustomFontBug
+//
+//  Created by Jason Ji on 8/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaceholderCustomFontBug/PlaceholderCustomFontBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceholderCustomFontBugApp.swift
+//  PlaceholderCustomFontBug
+//
+//  Created by Jason Ji on 8/1/20.
+//
+
+import SwiftUI
+
+@main
+struct PlaceholderCustomFontBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
