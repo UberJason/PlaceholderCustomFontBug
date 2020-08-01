cb565c901eb673850aaadafea87beea2deccc2d9
Jason Ji <uberjason@gmail.com>
2020-08-01 13:28:50 -0400

    Added redaction for standard font and custom font to show how it renders question mark boxes.

--- PlaceholderCustomFontBug/ContentView.swift
+++ PlaceholderCustomFontBug/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack(alignment: .leading) {
+            Text("The first line below is the standard font, and redaction works fine.\nThe second line below uses a custom font, and redaction causes it to render as question mark boxes.")
+            VStack(alignment: .leading) {
+                Text("Hello, world!").padding()
+                Text("Hello Lato").font(.custom("Lato-Black", size: 24))
+            }.redacted(reason: .placeholder)
+        }
     }
 }
 
